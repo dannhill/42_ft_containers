f670a89656e6a8a1bc947602dbc45fa15bfd81de
Danilo Medas <dmedas@e3r9p5.42roma.it>
2022-03-17 23:42:26 +0100

    added some logic to vector

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -5,12 +5,41 @@ template<typename T>
 class ft_vector
 {
 	public:
-		ft_vector(void);
-		virtual	~ft_vector(void);
-		ft_vector(ft_vector<T> & cpy);
+		ft_vector(void){
+			_arr = new T[0];
+			_size = 0;
+			_capacity = 0;
 
-		ft_vector & 
+			return;
+		}
+		virtual	~ft_vector(void){
+			delete [] _arr;
 		
+			return;
+		}
+		ft_vector(ft_vector<T> const & cpy) : _size(cpy.size()), _capacity(cpy.capacity()){
+			_arr = new T[_capacity];
+			for (int i(0); i < _capacity; i++)
+			{
+				if (i < _size)
+					_arr[i] = cpy[i];
+				else
+					_arr[i] = NULL;
+			}
+
+			return;
+		}
+
+		ft_vector & operator=(ft_vector<T> const & asn);
+	protected:
+		// protected vars
+		T	*_arr;
+		int	_size;
+		int	_capacity;
+		
+		// static const
+		static const int	_max_size(536870912);
+
 };
 
 }
