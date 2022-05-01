65194a4888c9469fae8a116480412178665ec6cd
VigorNuminis <medas.danilo@gmail.com>
2022-05-01 02:55:19 +0200

    implemented iterator_traits std class

--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -815,4 +815,33 @@ bool operator>=(const const_reverse_iterator<Iterator>& lhs, const const_reverse
 }
 #pragma endregion
 
+#pragma region Iterator Traits
+template <class Iterator>
+class iterator_traits{
+	typedef Iterator::difference_type difference_type;
+	typedef Iterator::value_type value_type;
+	typedef Iterator::pointer pointer;
+	typedef Iterator::reference reference;
+	typedef Iterator::iterator_category iterator_category;
+};
+
+template <class T>
+class iterator_traits<T*>{
+	typedef ptrdiff_t difference_type;
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef std::random_access_iterator_tag iterator_category;
+};
+
+template <class T>
+class iterator_traits<const T*>{
+	typedef ptrdiff_t difference_type;
+	typedef T value_type;
+	typedef const T* pointer;
+	typedef const T& reference;
+	typedef std::random_access_iterator_tag iterator_category;
+};
+#pragma endregion
+
 }
