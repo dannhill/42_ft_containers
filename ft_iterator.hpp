22afce9bcb76ad332a2701628470ddfeba8e377b
VigorNuminis <medas.danilo@gmail.com>
2022-05-11 16:01:41 +0200

    fixed syntax error(added typename)

--- ft_iterator.hpp
+++ ft_iterator.hpp
@@ -818,11 +818,11 @@ bool operator>=(const const_reverse_iterator<Iterator>& lhs, const const_reverse
 #pragma region Iterator Traits
 template <class Iterator>
 class iterator_traits{
-	typedef Iterator::difference_type difference_type;
-	typedef Iterator::value_type value_type;
-	typedef Iterator::pointer pointer;
-	typedef Iterator::reference reference;
-	typedef Iterator::iterator_category iterator_category;
+	typedef typename Iterator::difference_type difference_type;
+	typedef typename Iterator::value_type value_type;
+	typedef typename Iterator::pointer pointer;
+	typedef typename Iterator::reference reference;
+	typedef typename Iterator::iterator_category iterator_category;
 };
 
 template <class T>
